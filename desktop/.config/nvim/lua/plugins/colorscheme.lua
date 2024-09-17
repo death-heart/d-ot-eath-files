f3513cd8bf03f9f228594140f02e12d0d1fb1eac
Zion <ziong@unr.edu>
2024-09-17 01:14:51 -0700

    desktop nvim update

--- desktop/.config/nvim/lua/plugins/colorscheme.lua
+++ desktop/.config/nvim/lua/plugins/colorscheme.lua
@@ -15,5 +15,15 @@ return {
             -- load the colorscheme here
             vim.cmd([[colorscheme tokyonight]])
         end
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                disable_background = true,
+            })
+            -- vim.cmd([[colorscheme rose-pine]])
+        end
     }
 }
---
+++ desktop/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>wf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-g>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ww', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+    end
+}
