14dc7ba43c529ee16fbfc6ddab1111d6da102270
death <death@ArchBook>
2024-09-17 12:27:01 -0700

    update to laptop configs

--- laptop/.config/nvim/init.lua
+++ laptop/.config/nvim/init.lua
@@ -1,3 +1,4 @@
 require("bread.lazy")
 require("bread.set")
+require("bread.remap")
 print("shut the fuck up bag")
---
+++ laptop/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+        heading = { position = 'inline' },
+    },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+}
